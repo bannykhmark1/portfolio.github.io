cb447cbc3ed136a93cacdbbc7705c7df67b3b19b
bannykhmark1 <48414914+bannykhmark1@users.noreply.github.com>
2022-08-29 13:16:21 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+
+/*const iconMenu = document.querySelector('.menu-burger__header');
+if (iconMenu) {
+    const headerMenu = document.querySelector('header__menu _container');
+    iconMenu.addEventListener("click", function(e) {
+        iconMenu.classList.toggle('_active');
+        headerMenu.classList.toggle('_active');
+    });
+
+}*/
+
+const hamb = document.querySelector('#hamb');
+const popup = document.querySelector('#popup');
+const menu = document.querySelector('#menu').cloneNode(1);
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    renderPopup();
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
