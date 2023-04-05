e6b978a97c3735eb1d653562ccf542d5835950e5
bannykhmark1 <bann.mark@yandex.ru>
2023-04-05 13:59:38 +0500

    начал делать переводы текста

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+  document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+  document.getElementById("myForm").style.display = "none";
 }
-
 
 /*const iconMenu = document.querySelector('.menu-burger__header');
 if (iconMenu) {
@@ -17,43 +16,41 @@ if (iconMenu) {
 
 }*/
 
-const hamb = document.querySelector('#hamb');
-const popup = document.querySelector('#popup');
-const menu = document.querySelector('#menu').cloneNode(1);
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
 hamb.addEventListener("click", hambHandler);
 
 let x = 2;
 
 function switchImgAndMargin() {
-    if (x == 1) {
-        x++;
-        document.getElementById("imgToSwitch").src = "img/hamb.svg";
-        let headerMargin = document.querySelector('.header');
-        headerMargin.style.marginTop = '65px';
-    } else if (x == 2) {
-        document.getElementById("imgToSwitch").src = "img/cross.svg";
-        x = 1;
-        let headerMargin = document.querySelector('.header');
-        headerMargin.style.marginTop = '0px';
-    }
+  if (x == 1) {
+    x++;
+    document.getElementById("imgToSwitch").src = "img/hamb.svg";
+    let headerMargin = document.querySelector(".header");
+    headerMargin.style.marginTop = "65px";
+  } else if (x == 2) {
+    document.getElementById("imgToSwitch").src = "img/cross.svg";
+    x = 1;
+    let headerMargin = document.querySelector(".header");
+    headerMargin.style.marginTop = "0px";
+  }
 }
 
-
 function hambHandler(e) {
-    e.preventDefault();
-    popup.classList.toggle("popup_open");
-    hamb.classList.toggle("active");
-    renderPopup();
+  e.preventDefault();
+  popup.classList.toggle("popup_open");
+  hamb.classList.toggle("active");
+  renderPopup();
 }
 
 function renderPopup() {
-    const hasChild = popup.childNodes.length > 0;
+  const hasChild = popup.childNodes.length > 0;
 
-    if (hasChild) {
-        popup.innerHTML = '';
-    } else {
-        popup.appendChild(menu);
-        popup.appendChild(translate);
-    }
-
+  if (hasChild) {
+    popup.innerHTML = "";
+  } else {
+    popup.appendChild(menu);
+    popup.appendChild(translate);
+  }
 }
